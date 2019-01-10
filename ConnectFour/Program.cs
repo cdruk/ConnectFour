00a00e669780a9520dc858f51247f2fedd575edf
C Drukier <C Drukier@LAPTOP-OLP481GJ>
2019-01-10 15:21:08 -0500

    added first player option

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Configuration;
 
 namespace ConnectFour {
     internal class Program {
-        private static readonly int MAX_DEPTH = 4;
+        private static readonly int MAX_DEPTH = 2;
 
         private static void Main(string[] args) {
             if (args.Length > 0 && args[0].StartsWith("ON")) {
@@ -16,39 +15,53 @@ namespace ConnectFour {
             Console.WriteLine("MiniMax or AlphaBeta?");
             var gameType = Console.ReadLine();
 
+            Console.WriteLine("Computer or Player first?");
+            var firstPlayer = Console.ReadLine();
+
+            var nextPlayer = true;
+            if (firstPlayer.ToLower() == "computer") {
+                nextPlayer = true;
+            } else if (firstPlayer.ToLower() == "player") {
+                nextPlayer = false;
+            }
+
             while (!gameOver) {
                 if (gameType.ToLower() == "minimax") {
-                    Console.WriteLine("I am thinking about my move now");
-                    double highVal = -1.0;
-                    int bestMove = 0;
-                    for (int col = 0; col < Board.NR_COLS; ++col) {
-                        if (gameBoard.canMove(col)) {
-                            Board nextPos = gameBoard.MakeMove(Player.MAX, col);
-                            double thisVal = MiniMax.Value(nextPos, MAX_DEPTH, Player.MIN);
-                            if (thisVal > highVal) {
-                                bestMove = col;
-                                highVal = thisVal;
+                    if (nextPlayer == true) {
+                        Console.WriteLine("I am thinking about my move now");
+                        double highVal = -1.0;
+                        int bestMove = 0;
+                        for (int col = 0; col < Board.NR_COLS; ++col) {
+                            if (gameBoard.canMove(col)) {
+                                Board nextPos = gameBoard.MakeMove(Player.MAX, col);
+                                double thisVal = MiniMax.Value(nextPos, MAX_DEPTH, Player.MIN);
+                                if (thisVal > highVal) {
+                                    bestMove = col;
+                                    highVal = thisVal;
+                                }
                             }
                         }
-                    }
 
-                    if (highVal == -1) {
-                        bestMove = DesperationMove(gameBoard);
-                    }
-                    Console.WriteLine($"My move is {(bestMove + 1)}    (subj. value {highVal})");
-                    gameBoard = gameBoard.MakeMove(Player.MAX, bestMove);
-                    gameBoard.showBoard();
+                        if (highVal == -1) {
+                            bestMove = DesperationMove(gameBoard);
+                        }
+                        Console.WriteLine($"My move is {(bestMove + 1)}    (subj. value {highVal})");
+                        gameBoard = gameBoard.MakeMove(Player.MAX, bestMove);
+                        nextPlayer = !nextPlayer;
+                        gameBoard.showBoard();
 
-                    if (gameBoard.isWin(Player.MAX)) {
-                        Console.WriteLine("\n I win");
-                        Console.WriteLine("Value method called " + MiniMax.TimesValueCalled);
-                        gameOver = true;
-                    } else {
+                        if (gameBoard.isWin(Player.MAX)) {
+                            Console.WriteLine("\n I win");
+                            Console.WriteLine("Value method called " + MiniMax.TimesValueCalled);
+                            gameOver = true;
+                        }
+                    } else if (nextPlayer == false) {
                         line61_:
                         Console.WriteLine("Your move");
                         int theirMove = UserInput.getInteger("Select column 1 - 7", 1, 7) - 1;
                         if (gameBoard.canMove(theirMove)) {
                             gameBoard = gameBoard.MakeMove(Player.MIN, theirMove);
+                            nextPlayer = !nextPlayer;
                             Console.WriteLine("");
                             gameBoard.showBoard();
                         } else {
@@ -67,40 +80,44 @@ namespace ConnectFour {
                         }
                     }
                 } else if (gameType.ToLower() == "alphabeta") {
-                    Console.WriteLine("I am thinking about my move now");
-                    double highVal = -1.0;
-                    int bestMove = 0;
-                    double alfa = -1.0;
-                    double beta = 1.0;
-                    for (int col = 0; col < Board.NR_COLS; ++col) {
-                        if (gameBoard.canMove(col)) {
-                            Board nextPos = gameBoard.MakeMove(Player.MAX, col);
+                    if (nextPlayer == true) {
+                        Console.WriteLine("I am thinking about my move now");
+                        double highVal = -1.0;
+                        int bestMove = 0;
+                        double alfa = -1.0;
+                        double beta = 1.0;
+                        for (int col = 0; col < Board.NR_COLS; ++col) {
+                            if (gameBoard.canMove(col)) {
+                                Board nextPos = gameBoard.MakeMove(Player.MAX, col);
 
-                            double thisVal = AlphaBeta.Value(nextPos, MAX_DEPTH, alfa, beta, Player.MIN);
-                            if (thisVal > highVal) {
-                                bestMove = col;
-                                highVal = thisVal;
+                                double thisVal = AlphaBeta.Value(nextPos, MAX_DEPTH, alfa, beta, Player.MIN);
+                                if (thisVal > highVal) {
+                                    bestMove = col;
+                                    highVal = thisVal;
+                                }
                             }
                         }
-                    }
 
-                    if (highVal == -1) {
-                        bestMove = DesperationMove(gameBoard);
-                    }
-                    Console.WriteLine($"My move is {(bestMove + 1)}    (subj. value {highVal})");
-                    gameBoard = gameBoard.MakeMove(Player.MAX, bestMove);
-                    gameBoard.showBoard();
+                        if (highVal == -1) {
+                            bestMove = DesperationMove(gameBoard);
+                        }
+                        Console.WriteLine($"My move is {(bestMove + 1)}    (subj. value {highVal})");
+                        gameBoard = gameBoard.MakeMove(Player.MAX, bestMove);
+                        nextPlayer = !nextPlayer;
+                        gameBoard.showBoard();
 
-                    if (gameBoard.isWin(Player.MAX)) {
-                        Console.WriteLine("\n I win");
-                        Console.WriteLine("Value method called " + AlphaBeta.TimesValueCalled);
-                        gameOver = true;
-                    } else {
+                        if (gameBoard.isWin(Player.MAX)) {
+                            Console.WriteLine("\n I win");
+                            Console.WriteLine("Value method called " + AlphaBeta.TimesValueCalled);
+                            gameOver = true;
+                        }
+                    } else if (nextPlayer == false) {
                         line131_:
                         Console.WriteLine("Your move");
                         int theirMove = UserInput.getInteger("Select column 1 - 7", 1, 7) - 1;
                         if (gameBoard.canMove(theirMove)) {
                             gameBoard = gameBoard.MakeMove(Player.MIN, theirMove);
+                            nextPlayer = !nextPlayer;
                             Console.WriteLine("");
                             gameBoard.showBoard();
                         } else {
